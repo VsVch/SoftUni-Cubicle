bcfe7d2522e5a6753e0d4c44570d862f1105483a
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2023-01-20 23:57:52 +0200

    initializes port

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('initial');
+const express = require('express');
+const config = require('./config');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Home page');
+});
+
+app.listen(config.PORT, () => console.log(`Server is runinig on port ${config.PORT} ......np`));
