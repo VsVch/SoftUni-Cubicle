fe637fa8a42cb00f1fe7adad013ab4e2694c8340
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2023-01-22 16:04:23 +0200

    setup index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const hadlebars = require('express-handlebars');
+
 const config = require('./config');
 const app = express();
-
+app.engine('hbs', hadlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
 app.get('/', (req, res) => {
-    res.send('Home page');
+    res.render('home', {layout: false});
 });
 
 app.listen(config.PORT, () => console.log(`Server is runinig on port ${config.PORT} ......np`));
